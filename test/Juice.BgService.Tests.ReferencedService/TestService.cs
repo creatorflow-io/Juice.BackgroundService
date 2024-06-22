e627947dad9f7932e3e1b9a90a4faeeebb564b2b
ductran <ductran.tb@gmail.com>
2024-06-22 21:37:17 +0700

    Add document

---
+++ test/Juice.BgService.Tests.ReferencedService/TestService.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace Juice.BgService.Tests.ReferencedService
+{
+    public class TestService : BackgroundService
+    {
+        public TestService(ILogger<TestService> logger) : base(logger)
+        {
+        }
+        public override Task<(bool Healthy, string Message)> HealthCheckAsync() => Task.FromResult((true, string.Empty));
+        protected override Task ExecuteAsync()
+        {
+            _logger.LogInformation("TestService is running");
+            return Task.CompletedTask;
+        }
+    }
+}
